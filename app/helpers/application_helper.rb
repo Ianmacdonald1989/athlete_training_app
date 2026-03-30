b6742dcb434fe5e4c9f7e3eb05c7617c0fcb334a
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2026-03-30 12:44:50 +0100

    sport icon and card format updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+  def sport_icon(sport_definition)
+    sport = sport_definition.to_s.downcase
+
+    icon =
+      if sport.match?(/football|soccer/)
+        "⚽"
+      elsif sport.match?(/basketball/)
+        "🏀"
+      elsif sport.match?(/rugby/)
+        "🏉"
+      elsif sport.match?(/tennis/)
+        "🎾"
+      elsif sport.match?(/running|track|athletics|jog/)
+        "🏃"
+      elsif sport.match?(/cycling|bike/)
+        "🚴"
+      elsif sport.match?(/swim/)
+        "🏊"
+      elsif sport.match?(/gym|strength|weight|lifting/)
+        "🏋️"
+      elsif sport.match?(/golf/)
+        "⛳"
+      elsif sport.match?(/boxing/)
+        "🥊"
+      elsif sport.match?(/martial|karate|judo|taekwondo/)
+        "🥋"
+      elsif sport.match?(/cricket/)
+        "🏏"
+      elsif sport.match?(/hockey/)
+        "🏒"
+      else
+        "🏅"
+      end
+
+    tag.span(icon, class: "sport-icon", aria: { hidden: true })
+  end
 end
